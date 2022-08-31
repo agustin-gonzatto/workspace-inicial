5ddabb92be1c889011ddd513a467a10882f84c75
Agustin <110400139+agustin-gonzatto@users.noreply.github.com>
2022-08-31 16:29:42 -0300

    Update user.js

--- js/user.js
+++ js/user.js
@@ -2,5 +2,5 @@ let user = localStorage.getItem("user");
 let userIMG = localStorage.getItem("userIMG");
 document.addEventListener("DOMContentLoaded",()=>{
     document.getElementById("user").innerHTML+=`
-    <img src="${userIMG}"  size="20" height="20" width="20" class="circle">${user}`; 
+    <img src="${userIMG}"  size="20" height="20" width="20"> ${user}`; 
 });
