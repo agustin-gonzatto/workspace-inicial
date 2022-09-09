01bcaa6edaa544fddee5f4428d38db32011c074e
agustin-gonzatto <gonzattoagustin1@gmail.com>
2022-09-09 20:33:25 -0300

    .

--- js/product-info.js
+++ js/product-info.js
@@ -6,8 +6,16 @@ let comment = {};
 
 function showPageProduct(data) {
     document.getElementById("contenedor").innerHTML += `
-        <div>
-            <h2>${data.name}</h2><br>
+            <br><h2>${data.name}</h2><br>
+            <div>
+            <div class="containerIMG">
+                <div class="IMGgrande">
+                    <img id="bigIMG" src="${data.images[0]}">
+                </div>
+                <div id="contenedorIMG">
+                </div>
+            </div>
+            <div>
             <h4>Precio:</h4>
             <h6>${data.cost} ${data.currency}</h6><br>
             <h4>Descipcion:</h4>
@@ -16,15 +24,18 @@ function showPageProduct(data) {
             <h6>${data.category}</h6><br>
             <h4>Cantidad vendidos:</h4>
             <h6>${data.soldCount}</h6><br>
-            <h4>Imagenes ilustrativas</h4>
-        </div>
-        <div id="contenedorIMG">
-        </div>
+            <hr>
+            </div>
+            </div>
+            
+            
+       
+            
         
     `;
     data.images.forEach((element) => {
         document.getElementById("contenedorIMG").innerHTML += `
-        <img src="./${element}" width=${100 / data.images.length}%>
+        <img class="smallIMG" src="./${element}" width=${100 / data.images.length}% >
         `;
     });
 }
@@ -32,7 +43,7 @@ function showPageProduct(data) {
 function showComments(data) {
     data.forEach((element) => {
         document.getElementById("comments").innerHTML += `
-            <h6><b>${element.user}: </b>${element.description} - ${element.dateTime}</h6>
+            <h6><b>${element.user}: </b>${element.description} - </h6>
             `;
         for (let i = 0; i < element.score; i++) {
             document.getElementById("comments").innerHTML += `
@@ -44,49 +55,60 @@ function showComments(data) {
             <span class="fa fa-star"></span>
             `;
         }
-        document.getElementById("comments").innerHTML += `<br><br>`
+        document.getElementById("comments").innerHTML += `<p class="text-muted">${element.dateTime}<p>
+        <br>`;
     });
 }
 
-
-
 fetch(url.concat(localStorage.getItem("id")) + ".json")
     .then((res) => res.json())
-    .then((data) => {
-        showPageProduct(data);
+    .then((data1) => {
+        fetch(`https://japceibal.github.io/emercado-api/products_comments/${id}.json`)
+            .then((res) => res.json())
+            .then((data) => {
+                showPageProduct(data1);
+                showComments(data);
+            })
+            .then(() => {
+                console.log(document.getElementsByClassName("smallIMG").length);
+                for (let i = 0; i < document.getElementsByClassName("smallIMG").length; i++) {
+                    document.getElementsByClassName("smallIMG")[i].addEventListener("click", () => {
+                        document.getElementById("bigIMG").src = document.getElementsByClassName("smallIMG")[i].src;
+                    });
+                }
+            });
+        document.getElementById("commentBtn").addEventListener("click", () => {
+            let score;
+            for (let i = 0; i < document.getElementsByClassName("comment").length; i++) {
+                if (document.getElementsByClassName("comment")[i].classList.contains("checked")) {
+                    score = 5 - i;
+                }
+            }
+            let fecha = new Date();
+            comment = {
+                user: localStorage.getItem("user"),
+                description: document.getElementById("comment").value,
+                dateTime: fecha.toLocaleString("sv-SE"),
+                score: score,
+            };
+            comentario.push(comment);
+            showComments(comentario);
+            comentario = [];
+            for (let i = 0; i < document.getElementsByClassName("comment").length; i++) {
+                if (document.getElementsByClassName("comment")[i].classList.contains("checked")) {
+                    document.getElementsByClassName("comment")[i].classList.remove("checked");
+                }
+            }
+            document.getElementById("comment").value = "";
+            score = 0;
+        });
     });
 
-fetch(`https://japceibal.github.io/emercado-api/products_comments/${id}.json`)
-    .then((res) => res.json())
-    .then((data) => {
-        console.log(data);
-        showComments(data);
-    });
-
-document.getElementById("commentBtn").addEventListener("click", () => {
-    let score;
-
-    for (let i = 0; i < document.getElementsByClassName("comment").length; i++) {
-        if (document.getElementsByClassName("comment")[i].classList.contains("checked")) {
-            score = 5- i ;
-        }
-    }
-    let fecha = new Date();
-    comment = {
-        user: localStorage.getItem("user"),
-        description: document.getElementById("comment").value,
-        dateTime: fecha.toLocaleString("sv-SE"),
-        score: score,
-    };
-    comentario.push(comment);
-    showComments(comentario);
-    comentario = [];
-    for (let i = 0; i < document.getElementsByClassName("comment").length; i++) {
-        if (document.getElementsByClassName("comment")[i].classList.contains("checked")) {
-            document.getElementsByClassName("comment")[i].classList.remove("checked");
-        }
-    }
-    document.getElementById("comment").value = "";
-    score = 0;
-
-});
+/*
+        document.getElementsByClassName("smallIMG").forEach(element => {
+            element.addEventListener("click", () => {
+                console.log("hola")
+                document.getElementById("bigIMG").src = element.src;
+            })
+            
+        });*/
