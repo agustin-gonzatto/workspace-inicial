3f7b5ad7ca362ed3a69b8188dd9f8e08f18c6fe8
agustin-gonzatto <gonzattoagustin1@gmail.com>
2022-09-14 21:13:07 -0300

    Entrega 3

--- js/products.js
+++ js/products.js
@@ -6,84 +6,80 @@ let minCost = undefined;
 let maxCost = undefined;
 let productos = document.getElementsByClassName("list-group-item");
 
-fetch (url.concat(localStorage.getItem("catID"))+".json")
-    .then (res => res.json())
-    .then (data =>{
+fetch(url.concat(localStorage.getItem("catID")) + ".json")
+    .then((res) => res.json())
+    .then((data) => {
         console.log(data);
         localStorage.setItem("json", JSON.stringify(data));
-        document.getElementById("title").innerHTML+=`
+        document.getElementById("title").innerHTML += `
         <h2>Productos</h2>
         <p class="lead">Verás aquí todos los productos de la categoría ${data.catName}</p>      
         `;
         showPage(data.products);
-        document.getElementById("sortDown").addEventListener("click",()=>{
-            showPage(sortCategories(DOWN, data.products))
+        document.getElementById("sortDown").addEventListener("click", () => {
+            showPage(sortCategories(DOWN, data.products));
         });
-        document.getElementById("sortUp").addEventListener("click",()=>{
-            showPage(sortCategories(UP, data.products))
+        document.getElementById("sortUp").addEventListener("click", () => {
+            showPage(sortCategories(UP, data.products));
         });
-        document.getElementById("rating").addEventListener("click",()=>{
-            showPage(sortCategories(RATING, data.products))
+        document.getElementById("rating").addEventListener("click", () => {
+            showPage(sortCategories(RATING, data.products));
         });
 
-        document.getElementById("rangeFilterCostProducts").addEventListener("click", function(){
+        document.getElementById("rangeFilterCostProducts").addEventListener("click", function () {
             //Obtengo el mínimo y máximo de los intervalos para filtrar por cantidad
             //de productos por categoría.
             minCost = document.getElementById("rangeFilterCostMin").value;
             maxCost = document.getElementById("rangeFilterCostMax").value;
-        
-            if ((minCost != undefined) && (minCost != "") && (parseInt(minCost)) >= 0){
+
+            if (minCost != undefined && minCost != "" && parseInt(minCost) >= 0) {
                 minCost = parseInt(minCost);
-            }
-            else{
+            } else {
                 minCost = undefined;
             }
-        
-            if ((maxCost != undefined) && (maxCost != "") && (parseInt(maxCost)) >= 0){
+
+            if (maxCost != undefined && maxCost != "" && parseInt(maxCost) >= 0) {
                 maxCost = parseInt(maxCost);
-            }
-            else{
+            } else {
                 maxCost = undefined;
             }
-        
+
             showPage(data.products);
         });
-        document.getElementById("clearCostFilter").addEventListener("click",()=>{
-            maxCost=undefined;
-            minCost=undefined;
-            document.getElementById("rangeFilterCostMax").value ="";
-            document.getElementById("rangeFilterCostMin").value ="";
+        document.getElementById("clearCostFilter").addEventListener("click", () => {
+            maxCost = undefined;
+            minCost = undefined;
+            document.getElementById("rangeFilterCostMax").value = "";
+            document.getElementById("rangeFilterCostMin").value = "";
             showPage(data.products);
         });
 
-        document.addEventListener("keyup",e=>{
-            if (e.target.matches("#search")){
+        document.addEventListener("keyup", (e) => {
+            if (e.target.matches("#search")) {
                 for (let i = 0; i < data.products.length; i++) {
                     const element = data.products[i];
-                    var products = data.products.filter((element)=>element.name.toLowerCase().includes(e.target.value.toLowerCase()) | element.description.toLowerCase().includes(e.target.value.toLowerCase()));  
-                   
+                    var products = data.products.filter(
+                        (element) => element.name.toLowerCase().includes(e.target.value.toLowerCase()) | element.description.toLowerCase().includes(e.target.value.toLowerCase())
+                    );
                 }
-            showPage(products);
+                showPage(products);
             }
-                
-        
         });
 
+        //Guardar id clickeado y redireccionar
         for (let i = 0; i < productos.length; i++) {
             productos[i].addEventListener("click", () => {
+                localStorage.setItem("id", data.products[i].id);
                 window.location.href = "./product-info.html";
-                localStorage.setItem("id",data.products[i].id);
             });
         }
+    });
 
-    }); 
-
-function showPage(data){
-    document.getElementById("product-c").innerHTML ="";
+function showPage(data) {
+    document.getElementById("product-c").innerHTML = "";
     for (let i = 0; i < data.length; i++) {
-        if (((minCost == undefined) || (minCost != undefined && parseInt(data[i].cost) >= minCost)) &&
-        ((maxCost == undefined) || (maxCost != undefined && parseInt(data[i].cost) <= maxCost))){
-        document.getElementById("product-c").innerHTML +=`
+        if ((minCost == undefined || (minCost != undefined && parseInt(data[i].cost) >= minCost)) && (maxCost == undefined || (maxCost != undefined && parseInt(data[i].cost) <= maxCost))) {
+            document.getElementById("product-c").innerHTML += `
         <div class="list-group-item list-group-item-action cursor-active ">
             <div class="row">
                 <div class ="col-3">
@@ -98,45 +94,48 @@ function showPage(data){
                         </div>
                     </div>        
                 </div>
-                `  
-        }   
-    } 
-};
-
-
-
-
-
-
-function sortDown(a){
-
+                `;
+        }
+    }
 }
 
-function sortCategories(criteria, array){
+function sortCategories(criteria, array) {
     let result = [];
 
-    if (criteria === UP){
-        result = array.sort(function(a, b) {
+    if (criteria === UP) {
+        result = array.sort(function (a, b) {
             let aCount = parseInt(a.cost);
             let bCount = parseInt(b.cost);
-            if ( aCount > bCount ){ return -1; }
-            if ( aCount < bCount ){ return 1; }
+            if (aCount > bCount) {
+                return -1;
+            }
+            if (aCount < bCount) {
+                return 1;
+            }
             return 0;
         });
-    }else if (criteria === DOWN){
-        result = array.sort(function(a, b) {
+    } else if (criteria === DOWN) {
+        result = array.sort(function (a, b) {
             let aCount = parseInt(a.cost);
             let bCount = parseInt(b.cost);
-            if ( aCount < bCount ){ return -1; }
-            if ( aCount > bCount ){ return 1; }
+            if (aCount < bCount) {
+                return -1;
+            }
+            if (aCount > bCount) {
+                return 1;
+            }
             return 0;
         });
-    }else if (criteria === RATING){
-        result = array.sort(function(a, b) {
+    } else if (criteria === RATING) {
+        result = array.sort(function (a, b) {
             let aCount = parseInt(a.soldCount);
             let bCount = parseInt(b.soldCount);
-            if ( aCount > bCount ){ return -1; }
-            if ( aCount < bCount ){ return 1; }
+            if (aCount > bCount) {
+                return -1;
+            }
+            if (aCount < bCount) {
+                return 1;
+            }
             return 0;
         });
     }
